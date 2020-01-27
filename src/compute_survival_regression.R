b8b59c1a363e52280d0d5fa9a217951116cfb2e5
Keith Hughitt <keith.hughitt@gmail.com>
2020-01-27 18:35:52 -0500

    Added survival regression script

---
+++ src/compute_survival_regression.R
@@ -0,0 +1,82 @@
+#!/bin/env Rscript
+#
+# Compute feature-phenotype survival regression model
+#
+suppressMessages(library(arrow))
+suppressMessages(library(survival))
+suppressMessages(library(tidyverse))
+
+source('src/utils.R')
+
+set.seed(1)
+
+# load feature data
+feat_dat <- load_data(snakemake@input$features)
+
+# get feature settings
+feat_level <- snakemake@wildcards$feature_level
+feat_type <- snakemake@wildcards$feature_type
+phenotype <- snakemake@wildcards$phenotype
+
+feat_config <- snakemake@params$config$features[[feat_level]][[feat_type]]
+
+# get phenotype settings
+phenotype <- snakemake@wildcards$phenotype
+pheno_config <- snakemake@params$config$phenotype[[phenotype]]
+
+# sample id column in phenotype file
+sample_id <- pheno_config$sample_id
+
+# association parameters
+assoc_params <- pheno_config$associations[[snakemake@wildcards$covariate]]$params
+
+# load phenotype data
+pheno_dat <- read_tsv(snakemake@input$phenotype, col_types = cols())
+
+# ensure that sample order is consistent between feature/pheno data
+ind <- match(colnames(feat_dat)[-1], pull(pheno_dat, sample_id))
+pheno_dat <- pheno_dat[ind, ]
+
+if (!all(colnames(feat_dat)[-1] == pull(pheno_dat, sample_id))) {
+  stop("Feature/phenotype sample IDs do not match!")
+}
+
+# create a version of the feature data without the id columns, for convenience
+feat_mat <- as.matrix(feat_dat[, -1])
+
+# get survival time and event data
+surv_time <- as.numeric(pull(pheno_dat, assoc_params$time))
+surv_event <- as.logical(pull(pheno_dat, assoc_params$event))
+
+# build cox regression models and record p-values
+surv <- survival::Surv(time = surv_time, event = surv_event)
+
+fits <- apply(feat_mat, 1, function(x) {
+  summary(survival::coxph(surv ~ x))
+})
+
+# extract cox regression p-values
+pvals <- as.numeric(unlist(lapply(fits, function(x) {
+  x$waldtest['pvalue']
+})))
+
+# store result
+feat_id_col <- colnames(feat_dat)[1]
+feat_ids <- pull(feat_dat, feat_id_col)
+
+res <- data.frame(feat_ids, pvals)
+
+colnames(res) <- c(feat_id_col, sprintf("%s_pval", snakemake@wildcards$covariate))
+
+# for microarray data which may include multiple gene symbols for a single
+# row (e.g. "ABC1 // ABC2 // ETC"), split each such entries into multiple
+# rows and then collapse duplicated gene symbols, keeping only the lowest
+# p-value for each symbol
+if ((feat_id_col == 'symbol') && (any(grepl('//', feat_ids)))) {
+  res <- res %>%
+    separate_rows(symbol, sep = " ?//+ ?") %>%
+    group_by(symbol) %>%
+    summarise_all(min)
+}
+
+arrow::write_parquet(res, snakemake@output[[1]], compression = 'ZSTD')
