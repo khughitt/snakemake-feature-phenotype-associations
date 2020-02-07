511964f1bab3fea9ea536334b7587e72b9a80813
Keith Hughitt <keith.hughitt@gmail.com>
2020-02-07 15:27:17 -0500

    Expanded on metadata output

--- src/association_metadata.py
+++ src/association_metadata.py
@@ -1,9 +1,11 @@
 #!/bin/env python
 #
-# Creates a phenotype metadata table using the information in the dataset yaml files
+# Creates a feature-phenotype association metadata table using the information in the
+# dataset yaml files
 #
 import yaml
 import pandas as pd
+from utils import load_data
 
 entries = []
 
@@ -11,14 +13,33 @@ for infile in snakemake.params['cfg_paths']:
     with open(infile) as fp:
         cfg = yaml.safe_load(fp)
 
-        for phenotype in cfg['phenotypes']['associations']:
-            pheno_cfg = cfg['phenotypes']['associations'][phenotype]
+        # iterate over dataset entries
+        for feat_level in cfg['features']: 
+            for feat_type in cfg['features'][feat_level]:
+                # load feature data 
+                feat_path = cfg['features'][feat_level][feat_type]
+                feat_dat = load_data(feat_path)
 
-            entry = [cfg['name'], phenotype, pheno_cfg['category'], pheno_cfg['method']]
+                for phenotype in cfg['phenotypes']['associations']:
+                    pheno_cfg = cfg['phenotypes']['associations'][phenotype]
 
-            entries.append(entry)
+
+                    entry = {
+                        "dataset": cfg['name'],
+                        "feature_level": feat_level,
+                        "feature_type": feat_type,
+                        "phenotype": phenotype,
+                        "num_samples": feat_dat.shape[1] - 1,
+                        "num_features": feat_dat.shape[0],
+                        "feature_path": feat_path,
+                        "phenotype_path": cfg['phenotypes']['path'],
+                        "category": pheno_cfg['category'],
+                        "method": pheno_cfg['method']
+                    }
+
+                    entries.append(entry)
                 
-df = pd.DataFrame(entries, columns=['dataset', 'phenotype', 'category', 'method'])
+df = pd.DataFrame(entries)
 
 df.to_feather(snakemake.output[0])
 
---
+++ src/utils.py
@@ -0,0 +1,19 @@
+"""
+Python utility functions
+"""
+import pandas as pd
+
+def load_data(infile):
+    """Loads a tabular data file from disk"""
+    if infile.endswith('.tsv') or infile.endswith('.tsv.gz'):
+        dat = pd.read_csv(infile, sep='\t')
+    elif infile.endswith('.parquet'):
+        dat = pd.read_parquet(infile)
+    elif infile.endswith('.feather'):
+        dat = pd.read_feather(infile)
+
+    return dat
+
+def maxabs(x):
+    """Returns the maximum absolute value of a set of values"""
+    return max(abs(x))
